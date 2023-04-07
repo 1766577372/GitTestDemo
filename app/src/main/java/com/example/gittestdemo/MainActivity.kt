790ee1e18be1c30d84483a3deaaf509f8743f44d
张淞鸣 <3481048782@qq.com>
2023-04-07 11:48:52 +0800

    实现了加一按钮的加一功能

--- app/src/main/java/com/example/gittestdemo/MainActivity.kt
+++ app/src/main/java/com/example/gittestdemo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gittestdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gittestdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    private var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mBinding.btnAdd1.setOnClickListener {
+            num++
+            mBinding.textnumber.text = num.toString()
+        }
     }
 }
